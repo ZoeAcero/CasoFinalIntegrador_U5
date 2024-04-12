d19e75226a2500a227bc0cce60bfef1e4a3e04c7
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 16:22:46 +0200

    a

--- src/main/java/Analisis_Genomico/ConteoGenes.java
+++ src/main/java/Analisis_Genomico/ConteoGenes.java
@@ -9,8 +9,19 @@ public class ConteoGenes extends JFrame {
     private JButton contarGenesButton;
     private JLabel resultadoLabel;
 
-    public ConteoGenesUI() {
+    public ConteoGenes() {
         setTitle("Conteo de Genes");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        secuenciaADNField = new JTextField(20);
+        contarGenesButton = new JButton("Contar Genes");
+        resultadoLabel = new JLabel();
+
+        contarGenesButton.addActionListener(e -> {
+            String secuenciaADN = secuenciaADNField.getText();
+            AnalisisGenomico analisis = new AnalisisGenomico(secuenciaADN);
+            int cantidadGenes = analisis.contarGenes();
+            resultadoLabel.setText("Cantidad de genes: " + cantidadGenes);
+        });
 
     }
