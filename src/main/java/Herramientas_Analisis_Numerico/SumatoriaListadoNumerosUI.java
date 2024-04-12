255e7ec38451fd7fab5eb04d6ab22cf5aab7df34
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 16:59:30 +0200

    a

--- src/main/java/Herramientas_Analisis_Numerico/SumatoriaListadoNumerosUI.java
+++ src/main/java/Herramientas_Analisis_Numerico/SumatoriaListadoNumerosUI.java
@@ -1,4 +1,13 @@
 package Herramientas_Analisis_Numerico;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumatoriaListadoNumerosUI {
+    private final JTextField rangoInicialField;
+    private final JTextField rangoFinalField;
+    private final JButton calcularButton;
+    private final JTextArea resultadoArea;
 }
