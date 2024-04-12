016ea9f84b88553b8be92c376fc126f970beffdd
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 16:48:59 +0200

    a

--- src/main/java/Analisis_Genomico/CalculoCombinaciones.java
+++ src/main/java/Analisis_Genomico/CalculoCombinaciones.java
@@ -1,4 +1,13 @@
 package Analisis_Genomico;
 
 public class CalculoCombinaciones {
+    private String modeloGenetico;
+
+    public CalculoCombinaciones(String modeloGenetico) {
+        this.modeloGenetico = modeloGenetico;
+    }
+
+    public int calcular() {
+        return modeloGenetico.length();
+    }
 }
