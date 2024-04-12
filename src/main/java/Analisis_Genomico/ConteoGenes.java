b09c550ae3d24d44b0ed11710bc1daeba56f92f6
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 16:21:59 +0200

    a

--- src/main/java/Analisis_Genomico/ConteoGenes.java
+++ src/main/java/Analisis_Genomico/ConteoGenes.java
@@ -5,5 +5,12 @@ import java.awt.*;
 
 public class ConteoGenes extends JFrame {
 
-    
-}
+    private JTextField secuenciaADNField;
+    private JButton contarGenesButton;
+    private JLabel resultadoLabel;
+
+    public ConteoGenesUI() {
+        setTitle("Conteo de Genes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
