5847f350e5cba5cb3c379547228a38b118b7493f
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 17:00:11 +0200

    a

--- src/main/java/Herramientas_Analisis_Numerico/SumatoriaListadoNumerosUI.java
+++ src/main/java/Herramientas_Analisis_Numerico/SumatoriaListadoNumerosUI.java
@@ -10,4 +10,15 @@ public class SumatoriaListadoNumerosUI {
     private final JTextField rangoFinalField;
     private final JButton calcularButton;
     private final JTextArea resultadoArea;
+
+    public SumatoriaListadoNumerosUI() {
+        setTitle("Sumatoria y Listado de Números");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        rangoInicialField = new JTextField(5);
+        rangoFinalField = new JTextField(5);
+        calcularButton = new JButton("Calcular");
+        resultadoArea = new JTextArea(10, 20);
+        resultadoArea.setEditable(false);
 }
