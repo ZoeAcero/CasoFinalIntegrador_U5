201c69cd455e847e3682677410fb2ee5851e763f
ZoeAcero <zar.amazona@gmail.com>
2024-04-12 16:40:55 +0200

    a

--- src/main/java/Analisis_Genomico/CalculoCombinacionesUI.java
+++ src/main/java/Analisis_Genomico/CalculoCombinacionesUI.java
@@ -1,4 +1,14 @@
 package Analisis_Genomico;
 
-public class CalculoCombinacionesUI {
-}
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculoCombinacionesUI extends JFrame {
+    private JTextField modeloGeneticoField;
+    private JButton calcularCombinacionesButton;
+    private JLabel resultadoLabel;
+
+    public CalculoCombinacionesUI() {
+        setTitle("Cálculo de Combinaciones Genéticas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
