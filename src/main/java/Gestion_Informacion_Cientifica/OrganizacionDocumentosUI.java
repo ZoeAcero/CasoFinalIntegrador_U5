a0f78172160d584958ab39a6cd27a191f733884b
ZoeAcero <zar.amazona@gmail.com>
2024-04-15 13:45:01 +0200

    a

---
+++ src/main/java/Gestion_Informacion_Cientifica/OrganizacionDocumentosUI.java
@@ -0,0 +1,12 @@
+package Gestion_Informacion_Cientifica;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+public class OrganizacionDocumentosUI {
+}
