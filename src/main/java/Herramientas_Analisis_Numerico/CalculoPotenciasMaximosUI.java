b1ea05352434c2b8d9c5e4761eb70dea6871854e
ZoeAcero <zar.amazona@gmail.com>
2024-04-15 13:33:41 +0200

    a

--- src/main/java/Herramientas_Analisis_Numerico/CalculoPotenciasMaximosUI.java
+++ src/main/java/Herramientas_Analisis_Numerico/CalculoPotenciasMaximosUI.java
@@ -3,4 +3,8 @@ package Herramientas_Analisis_Numerico;
 import javax.swing.*;
 import java.awt.*;
 public class CalculoPotenciasMaximosUI {
+    private JTextField baseField;
+    private JTextField exponenteField;
+    private JButton calcularButton;
+    private JTextArea resultadoArea;
 }
