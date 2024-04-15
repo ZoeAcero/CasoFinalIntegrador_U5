f3cdac667b079a8b59fb455f9a6abeb32e1e55e4
ZoeAcero <zar.amazona@gmail.com>
2024-04-15 13:51:31 +0200

    a

--- src/main/java/Gestion_Informacion_Cientifica/BusquedaEficienteTextosUI.java
+++ src/main/java/Gestion_Informacion_Cientifica/BusquedaEficienteTextosUI.java
@@ -4,5 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-public class BusquedaEficienteTextosUI {
-}
+public class BusquedaEficienteTextosUI extends JFrame{
+    private JTextField palabraBuscarField;
+    private JButton buscarButton;
+    private JTextArea textoArea;
+
+    public BusquedaEficienteTextosUI() {
+        setTitle("Búsqueda Eficiente en Textos");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
