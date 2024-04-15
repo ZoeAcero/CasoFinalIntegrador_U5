69828be045ff6b0cb8121a22ca622977c43fac5e
ZoeAcero <zar.amazona@gmail.com>
2024-04-15 13:53:31 +0200

    a

--- src/main/java/Gestion_Informacion_Cientifica/BusquedaEficienteTextosUI.java
+++ src/main/java/Gestion_Informacion_Cientifica/BusquedaEficienteTextosUI.java
@@ -57,4 +57,13 @@ public class BusquedaEficienteTextosUI extends JFrame{
         return indices;
     }
 
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            BusquedaEficienteTextosUI ui = new BusquedaEficienteTextosUI();
+            ui.setVisible(true);
+        });
     }
+}
+
+    
