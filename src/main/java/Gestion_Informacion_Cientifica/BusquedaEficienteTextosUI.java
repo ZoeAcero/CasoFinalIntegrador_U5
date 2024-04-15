add340da2c8f9b0456d9095c99c6d8b3ba21731a
ZoeAcero <zar.amazona@gmail.com>
2024-04-15 13:50:44 +0200

    a

---
+++ src/main/java/Gestion_Informacion_Cientifica/BusquedaEficienteTextosUI.java
@@ -0,0 +1,8 @@
+package Gestion_Informacion_Cientifica;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+public class BusquedaEficienteTextosUI {
+}
