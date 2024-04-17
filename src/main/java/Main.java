f95ccebf5503df565fa51e5f1ec2e88d693cde81
ZoeAcero <zar.amazona@gmail.com>
2024-04-17 04:22:52 +0200

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,7 @@ import Analisis_Genomico.*;
 import Gestion_Informacion_Cientifica.*;
 import Herramientas_Analisis_Numerico.*;
 import Optimizacion_Procesos.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
