6e67c5d4a8d4bbf4fa9adffadfb435bbc7c6213a
ZoeAcero <zar.amazona@gmail.com>
2024-04-17 04:25:28 +0200

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,21 @@ import java.awt.event.ActionListener;
 public class Main extends JFrame {
     public Main() {
 
+        // Personalización de colores para decorar :)
+        Color colorMarcoPrincipal = new Color(110, 180, 250, 98); // Azul raro
+        getContentPane().setBackground(colorMarcoPrincipal); // Establece el color de fondo del marco principal
+
         // Configurar el marco principal
         setTitle("Sistema Interactivo de Análisis Genómico y Organización de Datos (A.G.O.D.)");
         setSize(1000, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        
+        // Crear un panel para agregar componentes
+        JPanel panel = new DecoPanel();
+        panel.setLayout(new GridBagLayout()); // Usar GridBagLayout
+        panel.setBackground(new Color(255, 255, 255, 200)); // Blanco semi-transparente
+        panel.setOpaque(false); // Hacer que panelPrincipal sea
+        panel.setPreferredSize(new Dimension(getWidth(), getHeight())); // Establecer el tamaño del panel igual al del marco principal
+        add(panel);
     }
 
