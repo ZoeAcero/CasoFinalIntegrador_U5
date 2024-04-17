ab1bb9fba1a3dde0685443a2756aa77f43c309ba
ZoeAcero <zar.amazona@gmail.com>
2024-04-17 04:24:00 +0200

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,3 +7,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class Main extends JFrame {
+    public Main() {
+
+        // Configurar el marco principal
+        setTitle("Sistema Interactivo de Análisis Genómico y Organización de Datos (A.G.O.D.)");
+        setSize(1000, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        
+    }
+
